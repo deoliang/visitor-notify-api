f456d9736c34ee071546c74c8f4b8962764a7b73
Deon Liang <deonyuhaoliang@gmail.com>
2016-09-23 20:47:26 +0000

    test commit

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express()
 app.set('port',(process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello AAA')
 })
 
-app.listen(app.get('port'),function(){
-  console.log("app is running at"+app.get('port'))
+app.listen(app.get('port'), function(){
+  console.log("app is running at " + app.get('port'))
 });
